8dfbcc315d8b0ad34c52df1277f0b0e3f8a029ab
Jirlan <jirlansouza08@gmaial.com>
2022-06-12 12:42:03 -0400

    Created the Pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList(): JSX.Element {
@@ -193,6 +194,8 @@ export default function UserList(): JSX.Element {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
