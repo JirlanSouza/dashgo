54852fd64f8602df9ef16abe4a82391e8f3f8fb9
Jirlan <jirlansouza08@gmaial.com>
2022-07-04 22:08:30 -0400

    added the miragejs, faker library to manager faker api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../styles/theme";
 import { SideBarDrawerProvider } from "../components/contexts/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
+import { useEffect } from "react";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
